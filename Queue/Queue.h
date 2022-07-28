37e48fd5a15fddaddc6496f5071ec76ae0acb1b1
yves-819 <57142327+yves-819@users.noreply.github.com>
2022-07-28 17:57:38 +0800

    Create Queue.h

---
+++ Queue/Queue.h
@@ -0,0 +1,30 @@
+#pragma once
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+#include<stdbool.h>
+
+
+typedef int QDataType;
+typedef struct QueueNode
+{
+	QDataType data;
+	struct QueueNode* next;
+}QueueNode;
+typedef struct Queue
+{
+	QueueNode* head;
+	QueueNode* tail;
+}Queue;
+
+void QueueInit(Queue* pq);
+void QueueDestroy(Queue* pq);
+void QueuePush(Queue* pq, QDataType x)
+void QueuePop(Queue* pq);
+QDataType QueueFront(Queue* pq);
+QDataType QueueBack(Queue* pq);
+int Queuesize(Queue* pq);
+bool QueueEmpty(Queue* pq);
+
+
+
